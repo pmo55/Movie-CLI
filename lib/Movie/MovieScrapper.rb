bbf85968bc591a6ccac6d193786c82b3096d8406
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-17 03:00:13 +0000

    began work on methods

--- lib/Movie/MovieScrapper.rb
+++ lib/Movie/MovieScrapper.rb
@@ -1,8 +1,11 @@
 require 'open-uri'
 require 'pry'
 require 'nokogiri'
-
+require_relative './Movie.rb'
 class MovieScraper
+  def get_page
+    doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/browse/in-theaters/"))
+  end
   
-  
+
 end
