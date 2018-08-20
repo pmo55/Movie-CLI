3f287cf28a691584285983cba37306e7343a0c12
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-20 15:31:49 +0000

    added several lines of code for testing

--- lib/Movie/MovieScrapper.rb
+++ lib/Movie/MovieScrapper.rb
@@ -4,10 +4,13 @@ require 'nokogiri'
 require_relative './Movie.rb'
 class MovieScraper
   def get_page
-    Nokogiri::HTML(open("https://www.rottentomatoes.com/browse/in-theaters/"))
+   doc= Nokogiri::HTML(open("https://www.rottentomatoes.com/browse/in-theaters/"))
+      binding.pry
   end
   def get_movies
     self.get_page.search("h3").text
   end
-  binding.pry
 end
+MovieScraper.new.get_page
+
+
