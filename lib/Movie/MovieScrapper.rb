cd1d1c232d69d71da3e0e3c0680cc72f064d88c9
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-22 15:03:21 +0000

    change in website scrapped plus scrappers created

--- lib/Movie/MovieScrapper.rb
+++ lib/Movie/MovieScrapper.rb
@@ -5,6 +5,7 @@ require_relative './Movie.rb'
 class MovieScraper
   def get_page
    doc= Nokogiri::HTML(open("https://www.rottentomatoes.com/browse/in-theaters/"))
+   doc_2= Nokogiri::HTML(open("https://www.businessinsider.com/highest-grossing-movies-all-time-worldwide-box-office-2018-4"))
       binding.pry
   end
   def get_movies
@@ -15,3 +16,6 @@ MovieScraper.new.get_page
 
 
 # doc.css("div div div a").text.squeeze
+# doc_2.css(".slide").children.css("p").first.text=> "Global box office: $1.341 billion "
+# doc_2.css(".slide") => Title, studio, box office
+# doc_2.css(".slide").children.css(".image-source")=>movie studio
