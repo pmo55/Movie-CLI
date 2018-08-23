8a545f727b40daf25331d339a9b7d71d6a146e51
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-23 17:20:59 +0000

    building out menu method

--- lib/Movie/cli.rb
+++ lib/Movie/cli.rb
@@ -15,6 +15,16 @@ class Cli
   end
   def menu
     puts "please enter the corresponding number to the left of the movie for more info, or type in exit to leave application"
+    input=gets.strip
+    while input !=exit
+    Movie.all.each do |movie|
+      if movie.title.include?(input)
+        puts "Total box office sales: #{movie.total_box_office}"
+        puts "Studio: #{movie.studio}"
+        break
+      else put "please enter a valid entry"
+    end
+  end
   end
 end
 
