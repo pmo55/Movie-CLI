e6c9ed2a589ed7bb7566c44065e6743aea5c8c93
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-23 21:04:22 +0000

    changed menu method

--- lib/Movie/cli.rb
+++ lib/Movie/cli.rb
@@ -8,24 +8,22 @@ class Cli
     menu
   end
   def list_movies
+   @movies=[]
     puts "Top 10 box office movies of all time:"
     Movie.all.each.with_index(1) do |movie, i|
+      @movies << movie
       puts  "#{i}. #{movie.title}"
     end
+    binding.pry
   end
   def menu
     input= nil
+    while input != "exit"
     puts "please enter the corresponding number to the left of the movie for more info, or type in exit to leave application"
     input=gets.strip
-    while input != "exit"
-    Movie.all.each do |movie|
-      if movie.title.include?(input)
-        puts "Total box office sales: #{movie.total_box_office}"
-        puts "Studio: #{movie.studio}"
-        break
-      else put "please enter a valid entry"
-    end
-  end
+    puts @movies[input.to_i-1].title
+    puts @movies[input.to_i-1].studio
+    puts @movies[input.to_i-1].total_box_office
   end
 end
 end
